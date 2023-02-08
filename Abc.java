2a7e156cc700a1db131a6f2a4a093a34fa8c196f
manu123ammu <manasa.konduru26@gmail.com>
2023-02-08 18:21:24 +0530

    first commit

---
+++ Abc.java
@@ -0,0 +1,8 @@
+class Abc {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java!"); 
+        System.out.println("Second Line!"); 
+        System.out.println("Third Line!"); 
+        System.out.println("Fourth Line!"); 
+    }
+}
